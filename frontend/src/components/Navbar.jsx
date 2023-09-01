c098f7e6fa39f2a01235c3960ce5987572d0068e
Nikhil Tanwar <nikhiltanwarnt@gmail.com>
2023-08-31 22:36:52 -0400

    2. FrontEnd - FavIcon Chaange | Add Workout Form

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
     <header>
       <div className="container">
         <Link to="/">
-          <h1>💪🏽 Workout Buddy 🤸🏽‍♂️</h1>
+          <h1> Workout Buddy 🤸🏽‍♂️</h1>
         </Link>
       </div>
     </header>
