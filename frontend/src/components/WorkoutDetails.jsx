45af596601cb0d07df01bdef65e28b53a5a5a9e8
Nikhil Tanwar <nikhiltanwarnt@gmail.com>
2023-09-02 13:59:41 -0400

    2. FrontEnd - Delete API Setup

--- frontend/src/components/WorkoutDetails.jsx
+++ frontend/src/components/WorkoutDetails.jsx
@@ -1,6 +1,22 @@
 import React from "react";
+import { useWorkoutContext } from "../hooks/useWorkoutContext";
 
 const WorkoutDetails = ({ workout }) => {
+  const { dispatch } = useWorkoutContext();
+  const handleClick = async () => {
+    const response = await fetch("/api/workouts/" + workout._id, {
+      method: "DELETE",
+    });
+
+    const json = await response.json();
+
+    if (response.ok) {
+      dispatch({
+        type: "DELETE_WORKOUT",
+        payload: json,
+      });
+    }
+  };
   return (
     <div className="workout-details">
       <h4>{workout.title}</h4>
@@ -13,6 +29,7 @@ const WorkoutDetails = ({ workout }) => {
         {workout.reps}
       </p>
       <p>{workout.createdAt}</p>
+      <span onClick={handleClick}>❌</span>
     </div>
   );
 };
