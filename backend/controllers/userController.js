1184ac3facd3abc1464e24c64b82aad141f94b87
Nikhil Tanwar <nikhiltanwarnt@gmail.com>
2023-09-03 16:38:50 -0400

    3.3 Auth - JWT | SignUp | Login

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,11 +1,25 @@
 const User = require("../models/UserModel");
+const jwt = require("jsonwebtoken");
+
+const createToken = (_id) => {
+  return jwt.sign({ _id }, process.env.SECRET, { expiresIn: "1d" });
+};
 
 // login user
 
 const loginUser = async (req, res) => {
-  res.json({
-    message: "Login User",
-  });
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.login(email, password);
+
+    //create a token
+    const token = createToken(user._id);
+
+    res.status(200).json({ email, token });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
 };
 
 // signup user
@@ -14,7 +28,11 @@ const signupUser = async (req, res) => {
 
   try {
     const user = await User.signup(email, password);
-    res.status(200).json({ email, user });
+    //create a token
+
+    const token = createToken(user._id);
+
+    res.status(200).json({ email, token });
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
